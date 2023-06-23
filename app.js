991b7be47c06aea206f13a1dac5234f3e3fb79f3
Carolina Hernandez <hernandez.carolina@live.cl>
2023-06-22 22:05:09 -0400

    grafico covid

---
+++ app.js
@@ -0,0 +1,60 @@
+const container = document.getElementById('container')
+const pais = document.getElementById('inputPais')
+const boton = document.getElementById('boton')
+const formulario = document.getElementById('formulario')
+const tipo = document.getElementById('tipo')
+const canvas = document.getElementById('grafico')
+
+const oms = async (e) => {
+    e.preventDefault()
+    const country = pais.value
+    const type = tipo.value
+    const url = `https://api.api-ninjas.com/v1/covid19?country=${country}&type=${type}`
+    const { data } = await axios.get(url, {
+        headers: {
+            'X-Api-Key': '6BPR4z2jD4jpGwbHE9B8IQ==R206jBOff3JqFxdM'
+        }
+    })
+
+    console.log(data)
+    let dataGrafico ={}
+
+    if(type === 'cases'){
+        const fechas = Object.keys(data[0].cases);
+    const info = Object.values(data[0].cases).map((elemento) => elemento.total);
+    dataGrafico = {
+        fechas,
+        info,
+        type,
+    };
+    }
+    else if(type === 'deaths'){
+        const fechas = Object.keys(data[0].deaths);
+    const info = Object.values(data[0].deaths).map((elemento) => elemento.total);
+     dataGrafico = {
+        fechas,
+        info,
+        type,
+    };
+    }
+
+crearGrafico(dataGrafico);
+
+}
+boton.addEventListener('click', oms)
+let grafico;
+const crearGrafico = (data) => {
+    if(grafico){
+        grafico.destroy()
+    }
+ grafico = new Chart(canvas, {
+        type: 'bar',
+        data: {
+            labels: data.fechas,
+            datasets: [{
+                label: data.type,
+                data: data.info
+            }]
+        }
+    })
+}
